9ce66e652f15c8feb5a389960e0d65a22ba77fb0
Hirokazu Chiba <hchiba1@gmail.com>
2020-07-03 17:31:14 +0900

    refactor stanza/ortholog/index.js

--- stanza/ortholog/index.js
+++ stanza/ortholog/index.js
@@ -1,42 +1,31 @@
-Stanza(
-    function( stanza, params ) {
-        if( params.genes === null ) {
-            return;
-        }
-        var genes = params.genes.split( ' ' );
-        var genesStr = '';
-        genes.forEach(
-            function( gene ) {
-                genesStr = genesStr + ' "' + gene + '"';
-            }
-        );
-        params.genes = genesStr;
+Stanza((stanza, params) => {
+  if (params.genes === null) {
+    return;
+  }
+  var genes = params.genes.split( ' ' );
+  var genesStr = '';
+  genes.forEach((gene) => {
+    genesStr = genesStr + ' "' + gene + '"';
+  });
+  params.genes = genesStr;
 
-        var dataQuery = {
-            endpoint: 'https://sparql.orth.dbcls.jp/sparql',
-            template: 'data.rq',
-            parameters: params
-        };
+  var dataQuery = stanza.query({
+    endpoint: 'https://sparql.orth.dbcls.jp/sparql',
+    template: 'data.rq',
+    parameters: params
+  });
+  var countQuery = stanza.query({
+    endpoint: 'https://sparql.orth.dbcls.jp/sparql',
+    template: 'count.rq',
+  });
+  var query = Promise.all([dataQuery, countQuery]);
 
-        var countQuery = {
-                endpoint: 'https://sparql.orth.dbcls.jp/sparql',
-                template: 'count.rq',
-        };
-
-        var query = $.when( stanza.query( dataQuery ), stanza.query( countQuery ) );
-
-        query.then(
-            function( dataResult, countResult ) {
-                var rows = dataResult[ 0 ].results.bindings;
-                var count = countResult[ 0 ].results.bindings[ 0 ].max_id.value;
-                stanza.render(
-                    {
-                        template: 'stanza.html'
-                    }
-                );
-
-                ortholog.render( stanza.select( '#chart'), rows, genes, count );
-            }
-        );
-    }
-);
+  query.then(([dataResult, countResult]) => {
+    var rows = dataResult.results.bindings;
+    var count = countResult.results.bindings[0].max_id.value;
+    stanza.render({
+      template: 'stanza.html'
+    });
+    ortholog.render( stanza.select( '#chart'), rows, genes, count );
+  });
+});
