db98eb50cc9e5a3aea7b5664bd9e13c777c8fcbf
Hirokazu Chiba <hchiba1@gmail.com>
2020-07-03 17:42:53 +0900

    refactor stanza/ortholog/index.js

--- stanza/ortholog/index.js
+++ stanza/ortholog/index.js
@@ -2,30 +2,30 @@ Stanza((stanza, params) => {
   if (params.genes === null) {
     return;
   }
-  var genes = params.genes.split( ' ' );
-  var genesStr = '';
+  const genes = params.genes.split( ' ' );
+  let genesStr = '';
   genes.forEach((gene) => {
     genesStr = genesStr + ' "' + gene + '"';
   });
   params.genes = genesStr;
 
-  var dataQuery = stanza.query({
+  const dataQuery = stanza.query({
     endpoint: 'https://sparql.orth.dbcls.jp/sparql',
     template: 'data.rq',
     parameters: params
   });
-  var countQuery = stanza.query({
+  const countQuery = stanza.query({
     endpoint: 'https://sparql.orth.dbcls.jp/sparql',
     template: 'count.rq',
   });
-  var query = Promise.all([dataQuery, countQuery]);
+  const query = Promise.all([dataQuery, countQuery]);
 
   query.then(([dataResult, countResult]) => {
-    var rows = dataResult.results.bindings;
-    var count = countResult.results.bindings[0].max_id.value;
+    const rows = dataResult.results.bindings;
+    const count = countResult.results.bindings[0].max_id.value;
     stanza.render({
       template: 'stanza.html'
     });
-    ortholog.render( stanza.select( '#chart'), rows, genes, count );
+    ortholog.render(stanza.select('#chart'), rows, genes, count);
   });
 });
