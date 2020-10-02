ab7f83a7694a95aa38ab281cfa2d1078420f6a38
Hirokazu Chiba <hchiba1@gmail.com>
2020-10-02 14:34:26 +0900

    add setup.php

---
+++ hop/setup.php
@@ -0,0 +1,26 @@
+<?php
+    require_once 'db.php';
+
+    $file_name = __DIR__ . '\data\human_genes.tsv';
+    $dbh = openDb();
+    $sql = 'create table genes(id varchar, name varchar)';
+    $result = $dbh->query($sql);
+
+    $sql = "insert into genes(id, name) values(?, ?)";
+    $statement = $dbh->prepare($sql);
+
+    $file = file($file_name);
+    
+    foreach($file as $line) {
+        $tokens = explode("\t", $line);
+        $id = $tokens[0];
+        $name = str_replace("\n", '', $tokens[1]);
+        if(strlen($name) > 0) {
+            $statement->bindValue(1, $id, PDO::PARAM_STR);
+            $statement->bindValue(2, $name, PDO::PARAM_STR);
+            $statement->execute();
+        }
+    }
+    
+    $dbh = NULL;
+?>
