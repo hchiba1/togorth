32ab190cba6ef3c70b22d0b98c7774a82872d97f
Satoshi Tanaka <satstnka@gmail.com>
2020-10-08 15:02:44 +0900

    added database.

--- hop/js/ortholog-group.js
+++ hop/js/ortholog-group.js
@@ -222,10 +222,70 @@ ortholog.createGenesSelect = function( term ) {
     );
 }
 
+// create select
+ortholog.createGenesSelect2 = function( term ) {
+    $( '#gene-selection' ).select2(
+        {
+            ajax: {
+                type: 'POST',
+                url: 'get_genes2.php',
+                data: function(params) {
+                    data = {term: params.term};
+                    return data;
+                },
+                processResults: function(result) {
+                    array = [];
+                    var results = JSON.parse(result);
+                    results.forEach(
+                        function(element) {
+                            var label = element.label;
+                            var value = element.value;
+                            array.push(
+                                {
+                                    id: value,
+                                    text: (value + ' : ' + label)
+                                }
+                            );
+                        }
+                    )
+                    return { results: array };
+                }
+            },
+            tags: true
+        }
+    );
+}
+
 // create group page
 ortholog.createGroupPage = function() {
     ortholog.createGenesSelect();
-    $( '#gene-selection' ).change( 
+    $('#gene-selection').change( 
+        function() {
+            ortholog.onChangeGene();
+        }
+    );
+
+    $.ajax(
+       {
+           url: 'https://orth.dbcls.jp/sparql',
+           type: 'GET',
+           dataType: 'json',
+           data: {
+               format: 'application/sparql-results+json',
+               query: ortholog.createMaxQuery()
+           }
+       }
+    ).then(
+        function( result ) {
+            ortholog.maxId = parseInt( result.results.bindings[ 0 ].max_id.value );
+        }
+    );
+}
+
+// create group page
+ortholog.createGroupPage2 = function() {
+    ortholog.createGenesSelect2();
+    $('#gene-selection').change( 
         function() {
             ortholog.onChangeGene();
         }
