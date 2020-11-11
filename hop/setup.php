1b072e6eb1aaa1f807d32e068ce686fa6393439d
Hirokazu Chiba <hchiba1@gmail.com>
2020-11-11 17:07:25 +0900

    update hop/setup.php

--- hop/setup.php
+++ hop/setup.php
@@ -1,26 +1,25 @@
 <?php
-    require_once 'db.php';
 
-    $file_name = __DIR__ . '\data\human_genes.tsv';
-    $dbh = openDb();
-    $sql = 'create table genes(id varchar, name varchar)';
-    $result = $dbh->query($sql);
+$dbh = new PDO("sqlite:" . __DIR__ . "/data/human_genes.db");
+$sql = 'create table genes(id varchar, name varchar)';
+$result = $dbh->query($sql);
 
-    $sql = "insert into genes(id, name) values(?, ?)";
-    $statement = $dbh->prepare($sql);
+$sql = "insert into genes(id, name) values(?, ?)";
+$statement = $dbh->prepare($sql);
 
-    $file = file($file_name);
-    
-    foreach($file as $line) {
-        $tokens = explode("\t", $line);
-        $id = $tokens[0];
-        $name = str_replace("\n", '', $tokens[1]);
-        if(strlen($name) > 0) {
-            $statement->bindValue(1, $id, PDO::PARAM_STR);
-            $statement->bindValue(2, $name, PDO::PARAM_STR);
-            $statement->execute();
-        }
+$file_name = __DIR__ . '/data/human_genes.tsv';
+$file = file($file_name);
+
+foreach($file as $line) {
+    $tokens = explode("\t", $line);
+    $id = $tokens[0];
+    $name = str_replace("\n", '', $tokens[1]);
+    if(strlen($name) > 0) {
+        $statement->bindValue(1, $id, PDO::PARAM_STR);
+        $statement->bindValue(2, $name, PDO::PARAM_STR);
+        $statement->execute();
     }
-    
-    $dbh = NULL;
+}
+
+$dbh = NULL;
 ?>
