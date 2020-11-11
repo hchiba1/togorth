4dfa445c69155dc3e874d40d969e633dc19cd1eb
Hirokazu Chiba <hchiba1@gmail.com>
2020-11-11 16:51:42 +0900

    update get_genes2.php

--- hop/get_genes2.php
+++ hop/get_genes2.php
@@ -1,36 +1,34 @@
 <?php
-    require_once "db.php";
+$dbh = new PDO("sqlite:" . __DIR__ . "/data/human_genes2.db");
 
-    $dbh = openDb2();
+$term = '';
+if(isset($_REQUEST['term'])) {
+    $term = strtolower($_REQUEST['term']);
+}
 
-    $term = '';
-    if(isset($_REQUEST['term'])) {
-        $term = strtolower($_REQUEST['term']);
-    }
+$array = [];
 
-    $array = [];
+$sql = "select label, name from genes";
+if(strlen($term) > 0) {
+    $sql = $sql . " where name like '%" . $term . "%'";
+    $sql = $sql . " or label like '%" . $term . "%'";
+}
+$sql = $sql . " order by name limit 50";
 
-    $sql = "select label, name from genes";
-    if(strlen($term) > 0) {
-        $sql = $sql . " where name like '%" . $term . "%'";
-        $sql = $sql . " or label like '%" . $term . "%'";
-    }
-    $sql = $sql . " order by name limit 50";
+$result = $dbh->query($sql);
 
-    $result = $dbh->query($sql);
+foreach($result as $row) {        
+    array_push(
+        $array, 
+        array(
+            'label' => $row['name'], 
+            'value' => $row['label']
+        )
+    );
+}
 
-    foreach($result as $row) {        
-        array_push(
-            $array, 
-            array(
-                'label' => $row['name'], 
-                'value' => $row['label']
-            )
-        );
-    }
-    
-    $json = json_encode($array);
-    echo $json;
+$json = json_encode($array);
+echo $json;
 
-    $dbh = NULL;
+$dbh = NULL;
 ?>
