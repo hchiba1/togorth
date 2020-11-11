7c9750e960039259fd17fc85625c8f96ff34b180
Hirokazu Chiba <hchiba1@gmail.com>
2020-11-11 17:10:34 +0900

    update hop/get_genes.php

--- hop/get_genes.php
+++ hop/get_genes.php
@@ -1,29 +1,27 @@
 <?php
-    require_once "db.php";
+$dbh = new PDO("sqlite:" . __DIR__ . "/data/human_genes.db");
 
-    $dbh = openDb();
+$term = '';
+if(isset($_REQUEST['term'])) {
+    $term = strtolower($_REQUEST['term']);
+}
 
-    $term = '';
-    if(isset($_REQUEST['term'])) {
-        $term = strtolower($_REQUEST['term']);
-    }
+$array = [];
 
-    $array = [];
+$sql = "select name from genes";
+if(strlen($term) > 0) {
+    $sql = $sql . " where name like '%" . $term . "%'";
+}
+$sql = $sql . " limit 50";
 
-    $sql = "select name from genes";
-    if(strlen($term) > 0) {
-        $sql = $sql . " where name like '%" . $term . "%'";
-    }
-    $sql = $sql . " limit 50";
+$result = $dbh->query($sql);
 
-    $result = $dbh->query($sql);
+foreach($result as $row) {
+    array_push($array, $row['name']);
+}
 
-    foreach($result as $row) {
-        array_push($array, $row['name']);
-    }
-    
-    $json = json_encode($array);
-    echo $json;
+$json = json_encode($array);
+echo $json;
 
-    $dbh = NULL;
+$dbh = NULL;
 ?>
