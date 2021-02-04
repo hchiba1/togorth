489671d60e6d31e2c07b0b4e6eb913aeb9e59142
Hirokazu Chiba <hchiba1@gmail.com>
2021-02-04 12:09:48 +0900

    update index.js according to index.ts

--- hop_api/index.js
+++ hop_api/index.js
@@ -7,8 +7,8 @@ var express_1 = __importDefault(require("express"));
 var sqlite3_1 = __importDefault(require("sqlite3"));
 var app = express_1.default();
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 app.use(express_1.default.json());
@@ -16,9 +16,9 @@ app.use(express_1.default.urlencoded({ extended: true }));
 var router = express_1.default.Router();
 router.get('/api/genes', function (req, res) {
     var term = req.query.keyword;
-    var db = new sqlite3_1.default.Database("human_genes.db");
+    var db = new sqlite3_1.default.Database('human_genes.db');
     db.serialize(function () {
-        var sql = "SELECT name FROM genes";
+        var sql = 'SELECT name FROM genes';
         if (term !== undefined && term !== null && term !== '') {
             sql = sql + " WHERE name like '%" + term + "%'";
         }
